70b2feb98a1bcd86ce9984776e85a734f9ef6a8a
Dani Jaber <72860970+DaniJa03@users.noreply.github.com>
2022-05-31 16:12:49 +0200

    Add files via upload

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <nav class="container flex justify-around py-8 mx-auto bg-white">
+      <div>
+        <h3 class="text-2xl font-medium text-blue-500">FOOTCLUB</h3>
+      </div>
+      <div class="space-x-8">
+        <a href="">Spieler auflisten</a>
+        <a href="">Trainer auflisten</a>
+        <a href="">Medizinpersonal auflisten</a>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Hinzufügen
+        </button>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script></script>

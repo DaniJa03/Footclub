14f6794cecdf0116d57576dda09dfa9b6f4c7ca8
Dani Jaber <72860970+DaniJa03@users.noreply.github.com>
2022-05-31 21:00:10 +0200

    added

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,24 @@
+<script>
+import { RouterLink, createWebHistory, RouterView } from 'vue-router';
+</script>
 <template>
   <div>
     <nav class="container flex justify-around py-8 mx-auto bg-white">
       <div>
-        <h3 class="text-2xl font-medium text-blue-500">FOOTCLUB</h3>
+        <a href="">
+          <h3 class="text-2xl font-medium text-blue-500"><RouterLink to="/">FOOTCLUB</RouterLink></h3>
+        </a>
       </div>
       <div class="space-x-8">
-        <a href="">Spieler auflisten</a>
-        <a href="">Trainer auflisten</a>
-        <a href="">Medizinpersonal auflisten</a>
+        <a href="">
+          <RouterLink to="/spieler">Spieler auflisten</RouterLink>
+        </a>
+        <a href="">
+          <RouterLink to="/trainer">Trainer auflisten</RouterLink>
+        </a>
+        <a href="">
+          <RouterLink to="/medizinpersonal">Medizinpersonal auflisten</RouterLink>
+        </a>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Hinzufügen
         </button>
@@ -15,5 +26,3 @@
     </nav>
   </div>
 </template>
-
-<script></script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import SpielerView from '../views/SpielerView.vue';
+import TrainerView from '../views/TrainerView.vue';
+import MedizinPersonalView from '../views/MedizinPersonalView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +13,23 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/spieler',
+      name: 'spieler',
+      component: SpielerView,
+    },
+    {
+      path: '/trainer',
+      name: 'trainer',
+      component: TrainerView,
+    },
+    {
+      path: '/medizinpersonal',
+      alias: '/personal',
+      name: 'medizinpersonal',
+      component: MedizinPersonalView,
+    },
+
   ],
 });
 

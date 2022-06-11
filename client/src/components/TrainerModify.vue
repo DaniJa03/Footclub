f1fae73758285e9a78f11b0532f1350a2d160b2b
Dani Jaber <72860970+DaniJa03@users.noreply.github.com>
2022-06-11 12:51:19 +0200

    post und patch funktioniert noch nicht

---
+++ client/src/components/TrainerModify.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
+    <div class="sm:flex items-center justify-between">
+      <p
+        tabindex="0"
+        class="focus:outline-none text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800"
+      >
+        Trainer
+      </p>
+      <div>
+        <label>Trainerid</label>
+        <p>{{ sub.trainerid }}</p>
+      </div>
+      <div>
+        <label>Nachname</label>
+        <input
+          v-model="sub.nachname"
+          type="text"
+          name="floating_company"
+          id="floating_company"
+          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+      </div>
+      <div>
+        <label>Vorname</label>
+        <input
+          v-model="sub.vorname"
+          type="text"
+          name="floating_company"
+          id="floating_company"
+          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+      </div>
+      <div>
+        <label>Trainerrolle</label>
+        <input
+          v-model="sub.trainerrolle"
+          type="text"
+          name="floating_company"
+          id="floating_company"
+          class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          required
+        />
+      </div>
+      <div>
+        <button
+          @click="clear"
+          type="button"
+          class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+        >
+          Clear
+        </button>
+      </div>
+      <div>
+        <button
+          @click="emit('updateMe', sub)"
+          type="button"
+          class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Trainer updaten
+        </button>
+      </div>
+      <div>
+        <button
+          @click="emit('addSpieler', sub)"
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Neuer Trainer
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { watch, ref } from 'vue';
+
+const props = defineProps({
+  sub: Object,
+});
+
+const sub = ref({});
+
+watch(props, (newVal) => {
+  sub.value = { ...newVal.sub };
+});
+
+const clear = () => {
+  sub.value.trainerid = '';
+  sub.value.nachname = '';
+  sub.value.vorname = '';
+  sub.value.trainerrolle = '';
+};
+
+const spielerAddDisabled = () => {
+  const { spielerpassid, nachname, vorname, trikotnummer } = sub.value;
+  return spielerpassid || !(nachname && vorname && trikotnummer);
+};
+
+const emit = defineEmits(['addTrainer', 'updateMe']);
+</script>
---
+++ client/src/components/TrainerTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <body class="flex items-center justify-center py-8 px-2">
+      <div class="w-full sm:px-6">
+        <div class="bg-white shadow px-4 md:px-10 pt-4 md:pt-7 pb-5 overflow-y-auto">
+          <!-- <div v-for="(a, index) in spielerArray" :key="index">
+            <p>{{ a.spielerpassid }}</p>
+            <p>{{ a.vorname }}</p>
+            <p>{{ a }}</p>
+            <p>{{ a }}</p>
+          </div> -->
+          <table class="w-full whitespace-nowrap">
+            <thead>
+              <tr
+                tabindex="0"
+                class="focus:outline-none h-16 w-full text-sm leading-none text-gray-800"
+              >
+                <th class="font-normal text-left pl-4">Trainerid</th>
+                <th class="font-normal text-left pl-12">Vorname</th>
+                <th class="font-normal text-left pl-12">Nachname</th>
+                <th class="font-normal text-left pl-20">Trainerrolle</th>
+              </tr>
+            </thead>
+            <tbody class="w-full">
+              <tr
+                v-for="sub of subs"
+                :key="sub.trainerid"
+                tabindex="0"
+                class="focus:outline-none h-20 text-sm leading-none text-gray-800 bg-white hover:bg-gray-100 border-b border-t border-gray-100"
+              >
+                <td @click="emit('copy', sub)" class="pl-4 cursor-pointer">
+                  <div class="flex items-center">
+                    <p class="font-medium">{{ sub.trainerid }}</p>
+                  </div>
+                </td>
+                <td class="pl-12">
+                  <p class="text-sm font-medium leading-none text-gray-800">{{ sub.vorname }}</p>
+                </td>
+                <td class="pl-12">
+                  <p class="font-medium">{{ sub.nachname }}</p>
+                </td>
+                <td class="pl-20">
+                  <p class="font-medium">{{ sub.trainerrolle }}</p>
+                </td>
+                <td class="px-7 2xl:px-0">
+                  <button
+                    @click="emit('del', sub)"
+                    type="button"
+                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                  >
+                    Trainer löschen
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </body>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, computed, watch } from 'vue';
+import axios from 'axios';
+import { useSpielerStore } from '../stores/spielerStore.js';
+
+defineProps({
+  subs: Array,
+});
+
+const emit = defineEmits(['copy', 'del']);
+</script>
